142340c69cb726e83a7897952af2e8e925d863f0
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-04-05 15:12:18 -0300

    Correr el Job por API Rest

--- src/main/java/ar/com/laboratory/examplebatchapi/api/controller/JobController.java
+++ src/main/java/ar/com/laboratory/examplebatchapi/api/controller/JobController.java
@@ -5,22 +5,23 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.http.ResponseEntity;
 
-public interface JobController {
-    @Tag(name = "Example Batch API", description = "This is an example of an API that runs a batch process")
-    public interface ProductController {
+@Tag(name = "Example Batch API", description = "This is an example of an API that runs a batch process")
+public interface JobController{
 
-        @ApiResponse(
-                responseCode = "400",
-                description = "When the request have a field invalid we response this",
-                content = {
-                        @Content(mediaType = "application/json", schema = @Schema(implementation = RuntimeException.class))
-                }
-        )
-        @Operation(summary = "Return a list with Products")
-        public ResponseEntity<String> runJob();
-
-    }
+    @ApiResponse(
+            responseCode = "400",
+            description = "When the request have a field invalid we response this",
+            content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = RuntimeException.class))
+            }
+    )
+    @Operation(summary = "Run a job", description = "This operation runs a job that processes a list of products")
+    public ResponseEntity<String> runJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException;
 
 }
--- src/main/java/ar/com/laboratory/examplebatchapi/api/controller/impl/JobControllerImpl.java
+++ src/main/java/ar/com/laboratory/examplebatchapi/api/controller/impl/JobControllerImpl.java
@@ -2,10 +2,30 @@ package ar.com.laboratory.examplebatchapi.api.controller.impl;
 
 
 import ar.com.laboratory.examplebatchapi.api.controller.JobController;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-
-
+@RestController
+@RequestMapping("/api/v1/jobs")
+@AllArgsConstructor
 public class JobControllerImpl implements JobController {
 
+    JobLauncher jobLauncher;
+    Job job;
+    @Override
+    @GetMapping("/product/run")
+    public ResponseEntity<String> runJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        jobLauncher.run(job, new JobParameters());
+        return ResponseEntity.ok("OK");
+    }
 }
